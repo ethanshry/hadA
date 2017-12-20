0baaeffb16a9585b3467c757e142c6e174f0810b
Ethan Shry <ehshry@gmail.com>
2017-12-19 20:15:57 -0600

    API functions outline

--- API/apiMain.py
+++ API/apiMain.py
@@ -44,3 +44,81 @@ def checkForValidUsername(username):
 
 #application = default_app()
 print(checkForValidUsername('john'))
+
+
+import json
+#import pyodbc
+import pymssql
+from bottle import default_app, route, response
+
+serverCredentials = {
+    'server': "sql11.ezhostingserver.com",
+    'database': "HadA_Main",
+    'username': "admin-main",
+    'password': "passtheword-1"
+}
+
+returnData = []
+#connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + serverCredentials['server'] + ';DATABASE=' + serverCredentials['database'] + ';UID=' + serverCredentials['username'] + ';PWD=' + serverCredentials['password'])
+#connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=sql11.exhostingserver.com;DATABASE=HadA_Main;UID=admin-main;PWD=passtheword-1')
+
+
+#cursor.execute("SELECT name, password FROM Users WHERE name='bill'")
+#rows = cursor.fetchall()
+#for row in rows:
+#    returnData.append(row)
+#print(returnData)
+
+@route('/get/verifyUser/<username>')
+def checkForValidUsername(username):
+    connection = pymssql.connect("sql11.ezhostingserver.com", "admin-main", "passtheword-1", "HadA_Main")
+    cursor = connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM Users WHERE name='" + username + "'")
+    res = cursor.fetchone()
+    connection.close()
+    print(res)
+    returnData = {
+        'isValid': False
+    }
+    #if (res[0] == 1):
+    #    returnData['isValid'] = True
+
+    response.content_type = 'application/json'
+    return json.dumps(returnData)
+
+#
+# FEED
+#
+
+@route('/get/feedItems/<currentIndex>/<numRequestedItems>/<filters>')
+def getFeedItems(currentIndex, numRequestedItems, filters):
+    # currentIndex: integer 0 -> inf, API should verify isn't past bounds of posts in existance
+    # numRequestedItems: integer 1 -> inf 
+    # filters: string, CSV for filters. user should be USER$:$username, category should be CATEGORY$:$categoryname
+    # should return next X numebr of posts adhering to the user specifications
+
+@route('/get/trendingItems/')
+def getTrendingItems():
+    # should grab some recently popular categories, terms, and users from the db and return an array of trendItem objects
+#
+# AUTHENTICATION
+#
+
+@route('/get/auth/verifyUser/<username>')
+def getUserAuthentication(username):
+    # username: String
+    # should verify user is a valid user. Return true/false
+
+@route('/get/auth/authenticateUser/<username>/<password>')
+def getUserAuthentication(username, password):
+    # username: String
+    # password: String
+    # should verify user credentals and authenticate them, returning true/false authenticated
+
+@route('/post/auth/createUser/<username>/<password>')
+def createNewUser(username, password):
+    # username: String
+    # password: String
+    # should insert user into user DB and return void. Request service should then call getUserAuthentication with the new credentials.
+
+application = default_app()
